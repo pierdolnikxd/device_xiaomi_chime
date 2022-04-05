20d5407474e9d7870f3ad7f3a191464a07f7d97b
Bruno Martins <bgcngm@gmail.com>
2022-04-05 10:34:38 +0100

    chime: Inherit from QTI FCM
    * Moved FCM under vintf
    
    Change-Id: I0bdd4d63b60a1e29cdd69e71f7268932a105c16b
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,7 +99,9 @@ TARGET_HALS_PATH ?= hardware/qcom-caf/$(TARGET_BOARD_PLATFORM)
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/framework_compatibility_matrix.xml \
+    $(COMMON_PATH)/vintf/framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/derp/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
