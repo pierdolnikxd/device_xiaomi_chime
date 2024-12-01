dcc06404fb248fab1c57b8da32f0ec6afac724a6
Yumi Yukimura <me.cafebabe@gmail.com>
2024-12-01 16:49:25 +0000

    chime: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- chime.mk
+++ chime.mk
@@ -147,8 +147,6 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := space
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     vendor.display.config@1.0 \
     vendor.display.config@1.5 \
     vendor.display.config@1.11.vendor \
@@ -168,9 +166,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.memtrack-service \
     vendor.qti.hardware.display.mapperextensions@1.0.vendor \
     vendor.qti.hardware.display.mapperextensions@1.1.vendor\
-    memtrack.bengal \
     gralloc.bengal \
     hwcomposer.bengal \
     libdisplayconfig \
