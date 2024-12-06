7ea1b077616749eca85cd42ccc9f3a7d86fc1d06
danielml <daniel@danielml.dev>
2024-12-06 10:41:02 +0000

    chime: chime.mk: Build xiaomi-telephony-stub To provide the required Xiaomi IMS methods Change-Id: I1e75476a0d2ed55b5eae2a0a2c6ca4c3c4e2c0f1 Signed-off-by: nnippon99 <adamayyad1999@hotmail.com>

--- chime.mk
+++ chime.mk
@@ -500,10 +500,12 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper-prd.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
-    telephony-ext
+    telephony-ext \
+    xiaomi-telephony-stub
 
 PRODUCT_BOOT_JARS += \
-    telephony-ext
+    telephony-ext \
+    xiaomi-telephony-stub
 
 # Touchscreen
 PRODUCT_PACKAGES += \
