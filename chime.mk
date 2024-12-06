a8b7c73db3d9cfc7964cae14e5e1ef4e9388a093
P.Adarsh Reddy <padarshr@codeaurora.org>
2024-12-06 09:30:56 +0000

    chime: Enforce android-S new launch debugfs restrictions
    Change-Id: I78878e1c4177c07d66b13e5659d4360252c5f87b
    Signed-off-by: nnippon99 <adamayyad1999@hotmail.com>

--- chime.mk
+++ chime.mk
@@ -136,6 +136,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# DebugFS
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
+
 # DEX
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := space
 
