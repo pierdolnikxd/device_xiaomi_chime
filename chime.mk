9217c5bed166a45e974f4dabdaa135b56ad3e1bd
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    chime: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- chime.mk
+++ chime.mk
@@ -327,6 +327,11 @@ PRODUCT_PACKAGES += \
     WifiResCommon
 
 # Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
 
